98e32e4b23266363b3a81f399f97c13bfd31f50a
modesayo <ogunkolamodesayo@gmail.com>
2024-10-21 14:06:31 +0100

    Your commit message here

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef} from "react";
 import { Switch } from "@mui/material";
 import ModalComponent from "./modal";
 
